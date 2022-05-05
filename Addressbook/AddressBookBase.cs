b62625cb950d9a40436214062a978c5bd2a27b18
Abhishek kumar singh <82291395+abhigeu9193@users.noreply.github.com>
2022-05-05 18:45:06 +0530

    Delete AddressBookBase.cs

--- Addressbook/AddressBookBase.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-
-public class AddressBookBase
-{
-    public void AddContactToAddressBook(Contact contact) => addressBook.Add(contact);
-}
